2de43e41cecec913679ee3f77de930d76b198449
Jonathan Ngo <jonathanngo96@gmail.com>
2022-06-27 00:11:52 -0700

    removed unecessary string formatting

--- project_files/app.py
+++ project_files/app.py
@@ -140,7 +140,7 @@ else:
     col2.metric(
         label = "Days since starting level",
         value = None if not value else f'{value} days',
-        delta = None if not delta else f'{delta:.1f} days vs. level {level_stats["level"] - 1:.1f}',
+        delta = None if not delta else f'{delta:.1f} days vs. level {level_stats["level"] - 1}',
         delta_color = level_stats["delta_color"]
     )
 
